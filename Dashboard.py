6db266d587d3b382920993e060bdaacaa6219da1
Rafael Petit <rpetit.dev@gmail.com>
2025-03-21 17:05:50 -0300

    V1.3.0

--- Dashboard.py
+++ Dashboard.py
@@ -67,43 +67,49 @@ with abas[2]:
                       variaveis_categoricas, desempenho_mapping)
     
 # Exibir informações sobre o projeto
-# Exibir informações sobre o projeto
 st.markdown("---")  # Linha divisória
 
-# Criando o layout do rodapé
-footer_col1, footer_col2, footer_col3 = st.columns([1, 2, 1])
+# Criando o layout do rodapé melhorado
+footer_col1, footer_col2, footer_col3 = st.columns([1, 1, 1])
 
 with footer_col1:
-    # Carregando e exibindo o logo da UNIP
+    # Coluna da instituição
     try:
         logo = "Logo.jpg"  # Caminho para o logo
         st.image(logo, width=100)
     except:
         st.write("**UNIP - Universidade Paulista**")
+    
+    st.markdown("""
+    <div style='color: #636363; margin-top: 10px;'>
+        <p><b>Universidade Paulista</b></p>
+        <p style='font-size: 13px;'>Campus Sorocaba</p>
+        <p style='font-size: 12px;'>Curso de Ciência da Computação</p>
+    </div>
+    """, unsafe_allow_html=True)
 
 with footer_col2:
-    # Informações sobre o projeto
+    # Informações sobre o projeto (centralizado)
     st.markdown("""
     <div style='text-align: center; color: #636363;'>
-        <p><b>Dashboard de Análise do ENEM 2023</b></p>
-        <p style='font-size: 14px;'>Desenvolvido para o projeto de Iniciação Científica</p>
+        <p style='font-size: 16px;'><b>Dashboard de Análise do ENEM 2023</b></p>
+        <p style='font-size: 14px;'>Projeto de Iniciação Científica</p>
+        <hr style='margin: 10px 0; border-color: #e0e0e0;'>
         <p style='font-size: 12px;'>© 2025 - Todos os direitos reservados</p>
+        <p style='font-size: 11px; margin-top: 10px;'>v1.3.0 - Atualizado em 21/03/2025</p>
     </div>
     """, unsafe_allow_html=True)
 
 with footer_col3:
-    # Informações sobre o autor
+    # Coluna da equipe (desenvolvedor e orientador)
     st.markdown("""
     <div style='text-align: right; color: #636363;'>
-        <p><b>Desenvolvido por:</b></p>
-        <p>Rafael Petit</p>
-        <p style='font-size: 12px;'>Curso de Ciência da Computação<br>RA: N777060</p>
+        <p style='font-size: 15px;'><b>Equipe</b></p>
+        <p style='font-size: 14px; margin-bottom: 2px;'><b>Desenvolvedor:</b></p>
+        <p style='font-size: 13px; margin-top: 0;'>Rafael Petit</p>
+        <p style='font-size: 12px; margin-top: 0;'>rpetit.dev@gmail.com</p>
+        <p style='font-size: 14px; margin-bottom: 2px; margin-top: 15px;'><b>Orientador:</b></p>
+        <p style='font-size: 13px; margin-top: 0;'>Prof. Dr. César C. Xavier</p>
+        <p style='font-size: 12px; margin-top: 0;'>cesarcx@gmail.com</p>
     </div>
     """, unsafe_allow_html=True)
-
-# Adicionar versão e contato
-st.markdown("""
-<div style='text-align: center; color: #9c9c9c; font-size: 11px; padding-top: 5px;'>
-    v1.0.0 | Para mais informações: rpetit.dev@gmail.com
-</div>
-""", unsafe_allow_html=True)
